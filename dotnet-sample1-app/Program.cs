df25b480a68871ff1f76a9c79dd0ed1b46bf4e9b
cgrigsby <grigsby.c@outlook.com>
2021-09-04 16:04:15 -0400

    initial commit

---
+++ dotnet-sample1-app/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace TV
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            Show one = new Show(){
+                Name = "Parks and Recreation",
+                NumEpisodes = 125, 
+                Year = 2009, 
+                Actors = new List<string>{
+                   "Amy Poehler", "Aziz Ansari", "Nick Offerman" 
+                }
+            };
+            Show two = new Show(){
+                Name = "Burn Notice",
+                NumEpisodes = 111, 
+                Year = 2007, 
+                Actors = new List<string>{
+                   "Jeffery Donovan", "Gabriel Anwar", "Bruce Cambell" 
+                }
+            };
+            Show three = new Show(){
+                Name = "The X-Files",
+                NumEpisodes = 217, 
+                Year = 1993, 
+                Actors = new List<string>{
+                   "David Duchovny", "Gillian Anderson", "Mitch Pileggi" 
+                }
+            };
+
+            List<Show> shows = new List<Show>(){
+                one, two, three
+            };
+            Console.WriteLine("-----Before Aplha Sort Sort------");
+            foreach(Show s in shows){
+                Console.WriteLine(s);
+            }
+
+            shows.Sort();
+            Console.WriteLine("-----After Aplha Sort------");
+            ;
+            foreach(Show s in shows){
+                Console.WriteLine(s);
+            }
+
+            shows.Sort(new NumEpisodeComparer());
+            Console.WriteLine("-----After Num of Episode Sort------");
+             foreach(Show s in shows){
+                Console.WriteLine(s);
+            }
+        }
+    }
+    public class NumEpisodeComparer: IComparer<Show>{
+        public int Compare(Show x, Show y){
+            return x.NumEpisodes - y.NumEpisodes;
+        }
+    }
+}
---
+++ if-loops-tutorial/Program.cs
@@ -0,0 +1,82 @@
+﻿using System;
+
+namespace BranchesAndLoops
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hola From Program.cs!");
+            //ExploreIf();
+
+            //While Loop
+            int counter = 0;
+            while (counter < 10)
+            {
+                Console.WriteLine($"While Loop: The counter is {counter}");
+                counter++;
+            }
+            // Do/While Loop
+            int counter2 = 0;
+            do
+            {
+                Console.WriteLine($"Do/While Loop: The counter is {counter}");
+                counter2++;
+            } while (counter2 < 10);
+
+            // For Loop
+            for (int index = 0; index < 10; index++)
+            {
+                System.Console.WriteLine($"For Loop: The index is {index}");
+            }
+
+            // Nested Loop
+            for (int row = 1; row < 11; row++)
+            {
+                for (char column = 'a'; column < 'k'; column++)
+                {
+                    Console.WriteLine($"The cell is ({row}, {column})");
+                }
+            }
+
+        }//main
+
+        static void ExploreIf()
+        {
+            Console.WriteLine("--------BranchesAndLoops----------");
+            int a = 5;
+            int b = 3;
+            if (a + b > 10)
+            {
+                Console.WriteLine("The answer is greater than 10.");
+            }
+            else
+            {
+                Console.WriteLine("The answer is less than 10.");
+            }
+            Console.WriteLine("------------------");
+            int c = 4;
+            if ((a + b + c > 10) && (a == b))
+            {
+                Console.WriteLine("The answer is greater than 10");
+                Console.WriteLine("And the first number is equal to the second");
+            }
+            else
+            {
+                Console.WriteLine("The answer is not greater than 10");
+                Console.WriteLine("Or the first number is not equal to the second");
+            }
+            Console.WriteLine("------------------");
+            if ((a + b + c > 10) || (a == b))
+            {
+                Console.WriteLine("The answer is greater than 10");
+                Console.WriteLine("Or the first number is equal to the second");
+            }
+            else
+            {
+                Console.WriteLine("The answer is not greater than 10");
+                Console.WriteLine("And the first number is not equal to the second");
+            }
+        }//ExploreIf()
+    }
+}
