160e6711641ca0f6ea36054df73d338aef2f3b3a
cgrigsby <grigsby.c@outlook.com>
2022-03-08 14:32:04 -0500

    feat(memoryManagement): add reference code from tutorial

---
+++ ValueVsRef/ValueAndRefTutorial/TutorialCode.cs
@@ -0,0 +1,24 @@
+//Code Graveyard
+
+// // Value Type
+// int x;
+// x = 10;
+// Console.WriteLine(x);
+// int y = x;
+// y = 20;
+// Console.WriteLine(y);
+
+
+// // Reference Type
+// Person p = new Person("Mary", "Scott");
+// Person q = p;
+// q.LastName = "Smith";
+// Console.WriteLine(p);
+// Console.WriteLine(q);
+// // q is an alias of p
+
+
+// string formattedPerson = FormatPerson(p);
+// Console.WriteLine(formattedPerson);
+// Console.WriteLine(p);
+// Console.WriteLine(q);
